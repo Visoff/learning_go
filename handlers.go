b3ae28eb01111b498737fc06814407fed9ee8540
Ilya <ikalinin01@mail.ru>
2023-07-02 20:39:50 +0600

    ...

---
+++ handlers.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+func InitHandlers() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("./public")))
+	mux.HandleFunc("/api/incriment", func(w http.ResponseWriter, r *http.Request) {
+		/*cookie, err := r.Cookie("token")
+		if err != nil {
+			http.SetCookie(w, &http.Cookie{Name: "token", Value: "1", Expires: time.Now().Add(365 * 24 * time.Hour)})
+			fmt.Println("error getting cookie, setting it up")
+		}*/
+		if r.Method == "POST" {
+			i++
+		} else if r.Method == "PATCH" {
+			body_bynary, _ := ioutil.ReadAll(r.Body)
+			body := string(body_bynary)
+			fmt.Println(body)
+			val, err := strconv.Atoi(body)
+			if err == nil {
+				i = val
+			}
+		}
+		w.Write([]byte(strconv.Itoa(i)))
+	})
+
+	return mux
+}
--- main.go
+++ main.go
@@ -3,28 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-	"time"
 )
 
 var i int = 0
 
 func main() {
-	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./public")))
-	mux.HandleFunc("/api/incriment", func(w http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie("token")
-		if err != nil {
-			http.SetCookie(w, &http.Cookie{Name: "token", Value: "1", Expires: time.Now().Add(365 * 24 * time.Hour)})
-			fmt.Println("error getting cookie, setting it up")
-		} else {
-			fmt.Println(cookie.Value)
-		}
-		if r.Method == "POST" {
-			i++
-		}
-		w.Write([]byte(strconv.Itoa(i)))
-	})
+	mux := InitHandlers()
 	fmt.Println("server is running on 8080")
 	http.ListenAndServe(":8080", mux)
 }
